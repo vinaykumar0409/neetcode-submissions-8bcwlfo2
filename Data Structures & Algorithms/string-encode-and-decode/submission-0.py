9af901c099d04540ddbe798624d5df747d9235f4
Vinay Kumar <66370247+vinaykumar0409@users.noreply.github.com>
2026-03-30 22:28:05 +0530

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        return '$$'.join(strs)
+
+    def decode(self, s: str) -> List[str]:
+        return s.split('$$')
+        
